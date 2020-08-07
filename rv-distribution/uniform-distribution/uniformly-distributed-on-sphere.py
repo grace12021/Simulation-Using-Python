52bf3e7fc34af4ad93bd93585391737d4a517749
grace12021 <grace12021@gmail.com>
2020-08-07 15:43:08 +0900

    Create uniformly-distributed-on-sphere.py

---
+++ rv-distribution/uniform-distribution/uniformly-distributed-on-sphere.py
@@ -0,0 +1,28 @@
+import numpy as np
+import csv
+import matplotlib.pyplot as plt
+
+# Generate N random points
+N = 1000
+
+#Generator of Uniform RV
+a = np.random.uniform(0, 1, N)
+b = np.random.uniform(0, 1, N)
+
+#Spherical Coordinates
+theta = 2 * np.pi * a
+phi = np.arccos(1-2*b)
+
+#XYZ Coordinates
+x = np.sin(phi) * np.cos(theta)
+y = np.sin(phi) * np.sin(theta)
+z = np.cos(phi)
+
+#Visuailization
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(x,y,z)
+
+#Save as png
+plt.savefig(str(N)+'-points-uniformly-distributed-on-sphere.png')
+plt.show()
